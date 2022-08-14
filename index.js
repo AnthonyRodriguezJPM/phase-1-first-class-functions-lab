18cbf6f67302640935dd0e68ffb9067624ca2c95
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-13 21:56:51 -0500

    commit tree 08eaed44536295faaec4514077892297e5694622

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Code your solution in this file!
+
+const array=['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers=function(){
+    const newArray=array.slice(0,2);
+    return newArray;
+}
+
+console.log(returnFirstTwoDrivers(array));
+console.log(array);
+
+//////////
+
+const returnLastTwoDrivers=function(){
+    const lastArray=array.slice(2);
+    return lastArray
+}
+
+console.log(returnLastTwoDrivers(array));
+console.log(array);
+
+//////////
+
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+    
+console.log(selectingDrivers[1]);
+
+//////////
+
+
+const createFareMultiplier=function(interger){
+    return function(fare) {
+        const newPrice=interger*fare;
+        return newPrice;
+    } 
+};
+
+console.log(createFareMultiplier(4)(4));
+
+///////////
+
+function fareDoubler(fare) {
+    const double=2*fare;
+    return double;
+};
+
+///////////
+
+function fareTripler(fare){
+    const triple=3*fare;
+    return triple;
+}
+
+////////
+function selectDifferentDrivers(array, selectNow) {
+    if(selectNow===returnFirstTwoDrivers) {
+        const hey=array.slice(0,2);
+        return hey;
+    }
+    else if(selectNow===returnLastTwoDrivers) {
+        const wow=array.slice(2);
+        return wow;
+    }
+}
+console.log(selectDifferentDrivers(array,returnLastTwoDrivers));
+
